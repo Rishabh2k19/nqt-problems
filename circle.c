97cb0b935f7a3fb238949a8d43298d95e732bee5
Rishabh2k19 <54538235+Rishabh2k19@users.noreply.github.com>
2020-10-21 11:22:35 +0530

    Create circle.c

---
+++ circle.c
@@ -0,0 +1,17 @@
+// Write a C program, to find the area of a circle when the diameter is given. The
+input diameter is an integer and the output area should be a floating point
+variable with 2 point precision.
+
+int main()
+{
+  
+  int dia;
+  float r, area;
+  scanf("%d",&dia);
+  r = (float)dia/2;
+ 
+  area = 3.14*r*r;
+  printf("%0.2f",area);
+
+  return 0;
+}
